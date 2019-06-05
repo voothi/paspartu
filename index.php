6a95a9bda174993be856bd4e530ebfbf73b7c2e7
Denis Novikov <voothi@gmail.com>
2019-06-06 01:19:48 +0300

    form

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="wrapper-form row justify-content-center">
+        <form>
+            <div class="form-group">
+                <label>Длина, мм</label>
+                <input class="form-control" type="text" name="dlina" placeholder="Длина, мм">
+                </label>
+            </div>
+
+            <div class="form-group">
+                <label>Ширина, мм</label>
+                <input class="form-control" type="text" name="shirina" placeholder="Ширина, мм">
+            </div>
+
+            <div class="form-group">
+                <label>Материал багета</label><br>
+                <label class="btn btn-info">
+                    <input type="radio" name="material-baget" autocomplete="off" checked> Серебро
+                </label>
+                <label class="btn btn-info">
+                    <input type="radio" name="material-baget" autocomplete="off"> Черный
+                </label>
+            </div>
+
+            <div class="form-group">
+                <label>Материал задника</label>
+                <select class="form-control" name="material-zadnik">
+                    <option value="df">sdf</option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Материал вставки</label>
+                <select class="form-control" name="material-vstavka">
+                    <option value="orgalit">Оргалит</option>
+                    <option value="pvh">ПВХ</option>
+                </select>
+            </div>
+        </form>
+    </div>
+    <script src="script.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
