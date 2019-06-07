4f00bb9a6068059db2fa4a9a95ecc6a33daa35c7
Denis Novikov <voothi@gmail.com>
2019-06-07 13:01:43 +0300

    str data

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 "use strict";
 
+material = {
+    "silver": 100,
+    "black": 200,
+    "hardboard": 100,
+    "pvc": 300,
+    "acrylic": 500,
+    "plexiglass": 500
+}
+
+material = {
+    "silver": {
+        "price": 100,
+        "unit": "m",
+        "amountUnit": 1,
+
+    },
+
+}
+
+const frameBaguette = [
+    {
+        "color": "silver",
+        "price": 100,
+        "unit": "m",
+        "amountUnit": 1,
+        "conditions"
+        frameBaguetteExternalLength
+
+    }
+]
+
+
+const frameBack = [
+    {
+        "color": "silver",
+        "price": 100,
+        "unit": "m",
+        "amountUnit": 1,
+        "conditions"
+        frameBaguetteExternalLength
+
+    }
+]
