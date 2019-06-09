78c4a7b315addc590e2fa1448ce894116d886a58
Denis Novikov <voothi@gmail.com>
2019-06-09 11:23:42 +0300

    params

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 "use strict";
 
-material = {
-    "silver": 100,
-    "black": 200,
-    "hardboard": 100,
-    "pvc": 300,
-    "acrylic": 500,
-    "plexiglass": 500
-}
+const formParams = {
+    "formParamsWidth": picInsert.picInsertWidth,
+    "formParamsLength": picInsert.picInsertLength,
+    // "formParamDepth": 0,
+    "formMaterial": 0,
+    "formParamDepth": 0,
+};
 
-material = {
-    "silver": {
-        "price": 100,
-        "unit": "m",
-        "amountUnit": 1,
+const picInsert = {
+    "picInsertLength": 0,
+    "picInsertWidth": 0,
+};
 
-    },
-
-}
+const frame = {
+    "frameExternalLength": 0,
+    "frameExternalWidth": 0,
+    "frameExternalWidth": 0,
+};
 
 const frameBaguette = [
     {
+        "manufacturer": "nelson",
         "color": "silver",
-        "price": 100,
-        "unit": "m",
-        "amountUnit": 1,
-        "conditions"
-        frameBaguetteExternalLength
-
-    }
-]
-
+        "price": {
+            "value": 100,
+            "currency": "rub",
+            "unit": "m",
+            "amountUnit": 1,
+            "physicalQuantity": "distance",
+        },
+        "frameBaguetteExternalCommonLength": 0, //общий погонаж багета по большей стороне
+        "frameBaguetteFacialWidth": 25, //ширина лицевой части багета
+        "frameBaguetteProfileWidth": 9, //толщина профиля багета
+        "frameBaguetteProfileHeight": 12 //высота профиля багета
+    },
+    {
+        "manufacturer": "nelson",
+        "color": "black",
+        "price": {
+            "value": 200,
+            "currency": "rub",
+            "unit": "m",
+            "amountUnit": 1,
+            "physicalQuantity": "distance",
+        },
+        "frameBaguetteExternalCommonLength": 0, //общий погонаж багета по большей стороне
+        "frameBaguetteFacialWidth": 25, //ширина лицевой части багета
+        "frameBaguetteProfileWidth": 9, //толщина профиля багета
+        "frameBaguetteProfileHeight": 12 //высота профиля багета
+    },
+];
 
 const frameBack = [
     {
@@ -39,7 +59,11 @@ const frameBack = [
         "unit": "m",
         "amountUnit": 1,
         "conditions"
-        frameBaguetteExternalLength
+        "frameBaguetteExternalLength": 0,
+        "frameBaguetteExternalWidth": 0,
 
     }
-]
+];
+
+const cart;
+amountFrameBaguetteInCart
