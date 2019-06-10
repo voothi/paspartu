e2288fcd5180249e4eb480e1ca387a047b650267
Denis Novikov <voothi@gmail.com>
2019-06-10 18:14:19 +0300

    probl w/let - func

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ let forms = document.forms[0];
 let buttonCalculate = forms.querySelector('div[name="calculate"]');
 
 function getOrder() {
-    // alert("еуые");
     order.picInsertExtHorizontal = forms.querySelector('input[name="picInsertExtHorizontal"]').value;
     order.picInsertExtVertical = forms.querySelector('input[name="picInsertExtVertical"]').value;
     order.frameBaguetteColor = forms.querySelector('select[name="frameBaguetteColor"]').value;
     order.frameBackMaterial = forms.querySelector('select[name="frameBackMaterial"]').value;
     order.frameInsertMaterial = forms.querySelector('select[name="frameInsertMaterial"]').value;
     order.frameAmount = forms.querySelector('input[name="frameAmount"]').value;
-    // console.log("test");
 }
 
 buttonCalculate.addEventListener("click", getOrder);
 
+console.log(order.picInsertExtHorizontal);
 
+let orderHorizontal = order.picInsertExtHorizontal;
+let orderVertical = order.picInsertExtVertical;
+let orderFrameBaguette = frameBaguette[order.frameBaguetteColor];
+let orderFrameBack = frameBack[order.frameBackMaterial];
+let orderFrameInsert = frameInsert[order.frameInsertMaterial];
+let orderFrameAmount = order.frameAmount;
 
+function calcPerimeterSquareFrameBaguette(calcOrderHorizontal, calcOrderVertical) {
+    return 2 * (calcOrderHorizontal + calcOrderVertical);
+}
+
+let orderFrameBaguetteCost = calcPerimeterSquareFrameBaguette(orderHorizontal, orderVertical) * orderFrameBaguette.price.value;
+// let orderFramePerimeter
