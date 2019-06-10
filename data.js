c108c967d2ceefa20836dc8e48c3dedee94791c5
Denis Novikov <voothi@gmail.com>
2019-06-10 22:25:45 +0300

    material

--- data.js
+++ data.js
@@ -3,14 +3,26 @@
 const order = {
     "picInsertExtHorizontal": 0,
     "picInsertExtVertical": 0,
+
     "frameBaguetteColor": 0,
-    "frameBaguettePrice": 0,
-    "frameBackMaterial": 0,
     "frameInsertMaterial": 0,
+    "frameBackMaterial": 0,
+
+    "frameBaguettePrice": 0,
+    "frameInsertPrice": 0,
+    "frameBackPrice": 0,
+
     "frameAmount": 1,
+
     "perimeterSquareFrameBaguette": 0,
     "areaSquarePicInsert": 0,
+
     "frameBaguetteCost": 0,
+    "frameInsertCost": 0,
+    "frameBackCost": 0,
+
+    "orderSumCost": 0,
+
     calcPerimeterSquareFrameBaguette: function () {
         this.perimeterSquareFrameBaguette = (this.picInsertExtHorizontal + this.picInsertExtVertical) * 2;
     },
@@ -18,14 +30,30 @@ const order = {
         this.areaSquarePicInsert = this.picInsertExtHorizontal * this.picInsertExtVertical;
     },
     getFrameBaguettePrice: function () {
-        this.frameBaguettePrice = (frameBaguette[this.frameBaguetteColor].price.value);
+        this.frameBaguettePrice = frameBaguette[this.frameBaguetteColor].price.value;
+    },
+    getFrameInsertPrice: function () {
+        this.frameInsertPrice = frameInsert[this.frameInsertMaterial].price.value;
+    },
+    getFrameBackPrice: function () {
+        this.frameBackPrice = (frameBack[this.frameBackMaterial].price.value);
     },
     calcFrameBaguetteCost: function () {
         this.frameBaguetteCost = this.frameAmount * (this.perimeterSquareFrameBaguette * this.frameBaguettePrice);
     },
-    convertRubPennyToRub: function (param1) {
-        return param1 / 100;
-    }
+    calcFrameInsertCost: function () {
+        this.frameInsertCost = this.frameAmount * (this.areaSquarePicInsert * this.frameInsertPrice);
+    },
+    calcFrameBackCost: function () {
+        this.frameBackCost = this.frameAmount * (this.areaSquarePicInsert * this.frameBackPrice);
+    },
+    calcOrderSumCost: function () {
+        this.orderSumCost = this.frameBaguetteCost + this.frameInsertCost + this.frameBackCost;
+    },
+    convertRubPennyToRub: function (valueInRubPenny) {
+        return valueInRubPenny / 100;
+    },
+
 };
 
 const picInsert = {
@@ -42,7 +70,10 @@ const frameBaguette = {
     "silver": {
         "manufacturer": "nelson",
         "material": "aluminium",
-        "color": "silver",
+        "color": {
+            "en": "silver",
+            "ru": "Серебро"
+        },
         "price": {
             "value": 10,
             "currency": "penny rub",
@@ -58,7 +89,10 @@ const frameBaguette = {
     "black": {
         "manufacturer": "nelson",
         "material": "aluminium",
-        "color": "black",
+        "color": {
+            "en": "black",
+            "ru": "Черный"
+        },
         "price": {
             "value": 20,
             "currency": "penny rub",
@@ -73,49 +107,19 @@ const frameBaguette = {
     },
 };
 
-const frameBack = {
-    "hardboard": {
-        "manufacturer": "nelson",
-        "material": "hardboard",
-        "color": "none",
-        "price": {
-            "value": 100,
-            "currency": "penny rub",
-            "unit": "mm^2",
-            "amountUnit": 1000000,
-            "physicalQuantity": "area",
-        },
-        "extVertical": 0,
-        "extHorizontal": 0,
-        "depth": 0,
-    },
-    "pvc": {
-        "manufacturer": "nelson",
-        "material": "pvc",
-        "color": "none",
-        "price": {
-            "value": 300,
-            "currency": "penny rub",
-            "unit": "mm^2",
-            "amountUnit": 1000000,
-            "physicalQuantity": "area",
-        },
-        "extVertical": 0,
-        "extHorizontal": 0,
-        "depth": 3, //mm
-    },
-};
-
 const frameInsert = {
     "acrylic": {
         "manufacturer": "nelson",
-        "material": "acrylic",
+        "material": {
+            "en": "acrylic",
+            "ru": "Акрил"
+        },
         "color": "transparent",
         "price": {
-            "value": 500,
+            "value": 50,
             "currency": "penny rub",
             "unit": "mm^2",
-            "amountUnit": 1000000,
+            "amountUnit": 1,
             "physicalQuantity": "area",
         },
         "extVertical": 0,
@@ -124,17 +128,59 @@ const frameInsert = {
     },
     "plexiglass": {
         "manufacturer": "nelson",
-        "material": "plexiglass",
+        "material": {
+            "en": "plexiglass",
+            "ru": "Оргстекло"
+        },
         "color": "transparent",
         "price": {
-            "value": 500,
+            "value": 50,
             "currency": "penny rub",
             "unit": "mm^2",
-            "amountUnit": 1000000,
+            "amountUnit": 1,
             "physicalQuantity": "area",
         },
         "extVertical": 0,
         "extHorizontal": 0,
         "depth": 0,
+    },
+};
+
+const frameBack = {
+    "hardboard": {
+        "manufacturer": "nelson",
+        "material": {
+            "en": "hardboard",
+            "ru": "Оргалит"
+        },
+        "color": "none",
+        "price": {
+            "value": 10,
+            "currency": "penny rub",
+            "unit": "mm^2",
+            "amountUnit": 1,
+            "physicalQuantity": "area",
+        },
+        "extVertical": 0,
+        "extHorizontal": 0,
+        "depth": "?",
+    },
+    "pvc": {
+        "manufacturer": "nelson",
+        "material": {
+            "en": "pvc",
+            "ru": "ПВХ"
+        },
+        "color": "none",
+        "price": {
+            "value": 30,
+            "currency": "penny rub",
+            "unit": "mm^2",
+            "amountUnit": 1,
+            "physicalQuantity": "area",
+        },
+        "extVertical": 0,
+        "extHorizontal": 0,
+        "depth": 3, //mm
     },
 };
--- script.js
+++ script.js
@@ -4,51 +4,71 @@ let forms = document.forms[0];
 let buttonCalculate = forms.querySelector('div[name="calculate"]');
 
 function getOrder() {
-    order.picInsertExtHorizontal = +forms.querySelector('input[name="picInsertExtHorizontal"]').value;
-    order.picInsertExtVertical = +forms.querySelector('input[name="picInsertExtVertical"]').value;
+    order.picInsertExtHorizontal = Number.parseInt(forms.querySelector('input[name="picInsertExtHorizontal"]').value);
+    order.picInsertExtVertical = Number.parseInt(forms.querySelector('input[name="picInsertExtVertical"]').value);
     order.frameBaguetteColor = forms.querySelector('select[name="frameBaguetteColor"]').value;
-    order.frameBackMaterial = forms.querySelector('select[name="frameBackMaterial"]').value;
+    // order.frameBaguetteColor = frameBaguette[order.frameBaguetteColor].color.ru;
+
     order.frameInsertMaterial = forms.querySelector('select[name="frameInsertMaterial"]').value;
-    order.frameAmount = forms.querySelector('input[name="frameAmount"]').value;
+    // let frameInsertElementFromStore = frameInsert[order.frameInsertMaterial];
+    // order.frameInsertMaterial = frameInsertElementFromStore.material.ru + " " + frameInsertElementFromStore.depth + " мм";
+
+    order.frameBackMaterial = forms.querySelector('select[name="frameBackMaterial"]').value;
+    // let frameBackElementFromStore = frameBack[order.frameBackMaterial];
+    // order.frameBackMaterial = frameBackElementFromStore.material.ru + " " + frameBackElementFromStore.depth + " мм";
+ 
+    order.frameAmount = Number.parseInt(forms.querySelector('input[name="frameAmount"]').value);
+
+    // if(order.picInsertExtHorizontal <) {
+
+    // }
 
     order.calcPerimeterSquareFrameBaguette();
-    order.calcAreaSquarePicInsert();
+
     order.getFrameBaguettePrice();
     order.calcFrameBaguetteCost();
-    // order.convertRubPennyToRub(order.calcFrameBaguetteCost);
 
-    console.log(order.convertRubPennyToRub(order.frameBaguetteCost));
+    order.calcAreaSquarePicInsert();
 
-    outResultBaguetteCost();
+    order.getFrameInsertPrice();
+    order.calcFrameInsertCost();
+
+    order.getFrameBackPrice();
+    order.calcFrameBackCost();
+
+    order.calcOrderSumCost();
+
+    outResult();
 }
 
 buttonCalculate.addEventListener("click", getOrder);
 
-// function resultBaguetteCost(par1, par2) {
-
-// }
-
-function outResultBaguetteCost(par1) {
+function outResult() {
     let resultDiv = document.querySelector('.result');
-    resultDiv.innerText = 
-`
+    resultDiv.innerText =
+        `
 Заказ:
-Тираж, шт.: ${order.frameAmount}
-- Багет
-Цвет: ${order.frameBaguetteColor},
-Цена (коп./кв. мм): ${order.frameBaguettePrice},
+Тираж: ${order.frameAmount} шт.
+- Багет -
+Цвет: ${order.frameBaguetteColor}
+Цвет: ${frameBaguette[order.frameBaguetteColor].color.ru}
+
+Цена: ${order.frameBaguettePrice} коп./мм
 Стоимость: ${order.convertRubPennyToRub(order.frameBaguetteCost)} руб.
-`;
+- Вставка -
+Материал: ${order.frameInsertMaterial}
+Материал: ${frameInsert[order.frameInsertMaterial].material.ru + " " + frameInsert[order.frameInsertMaterial].depth + " мм"}
+
+
+Цена: ${order.frameInsertPrice} коп./кв. мм 
+Стоимость: ${order.convertRubPennyToRub(order.frameBackCost)} руб.
+- Задник -
+Материал: ${order.frameBackMaterial}
+Материал: ${frameBack[order.frameBackMaterial].material.ru + " " + frameBack[order.frameBackMaterial].depth + " мм"}
+
+Цена: ${order.frameBackPrice} коп./кв. мм 
+Стоимость: ${order.convertRubPennyToRub(order.frameInsertCost)} руб.
+***
+Сумма: ${order.convertRubPennyToRub(order.orderSumCost)} руб.
+        `;
 }
-
-// console.log(order.picInsertExtHorizontal);
-
-// let orderHorizontal = order.picInsertExtHorizontal;
-// let orderVertical = order.picInsertExtVertical;
-// let orderFrameBaguette = frameBaguette[order.frameBaguetteColor];
-// let orderFrameBack = frameBack[order.frameBackMaterial];
-// let orderFrameInsert = frameInsert[order.frameInsertMaterial];
-// let orderFrameAmount = order.frameAmount;
-
-// let orderFrameBaguetteCost = calcPerimeterSquareFrameBaguette(order.picInsertExtHorizontal, order.picInsertExtVertical) * frameBaguette[order.frameBaguetteColor].price.value;
-// let orderFramePerimeter
